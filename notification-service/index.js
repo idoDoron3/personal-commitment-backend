da3450ea9aca0c8575cec6e08396b7e090879253
idoDoron3 <idodoron3011@gmail.com>
2024-12-28 11:58:30 +0200

    Initial setup for all microservices

---
+++ notification-service/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Service is running...');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Service running on port ${PORT}`);
+});
