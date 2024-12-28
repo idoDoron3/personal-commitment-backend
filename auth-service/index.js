2f519e484a7a1f209d8dc12a190c72167c7f559a
MatanVak23 <matanva@post.bgu.ac.il>
2024-12-28 15:35:21 +0200

    creating db working

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const sequelize = require('./config/db');
+const User = require('./models/User');
 const app = express();
+const PORT = process.env.SERVER_PORT;
 
-app.get('/', (req, res) => {
-  res.send('Service is running...');
-});
+require('dotenv').config(); // reload data from env file
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Service running on port ${PORT}`);
-});
+app.use(express.json());
+
+(async () => {
+    try {
+        await sequelize.sync(); 
+        console.log('Database synced successfully!');
+
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+})();
