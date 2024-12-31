7b195d7409d67c09f2792156a7b53b2d52c12f0a
MatanVak23 <matanva@post.bgu.ac.il>
2024-12-31 14:44:13 +0200

    syntax changed

--- auth-service/controllers/auth-controller.js
+++ auth-service/controllers/auth-controller.js
@@ -1,10 +1,16 @@
-const authService = require('../service/auth-Service');
+const authService = require("../service/auth-Service");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
     const { firstname, lastname, email, password } = req.body;
-    const user = await authService.registerUser(firstname, lastname, email, password);
-    res.status(201).json({ message: 'User registered successfully', user });
+    const user = await authService.registerUser(
+      firstname,
+      lastname,
+      email,
+      password
+    );
+    res.status(201).json({ message: "User registered successfully", user });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -23,8 +29,8 @@ exports.login = async (req, res) => {
 exports.logout = (req, res) => {
   try {
     // Invalidate the token (for stateless JWT, just inform the client to delete it)
-    res.status(200).json({ message: 'User logged out successfully' });
+    res.status(200).json({ message: "User logged out successfully" });
   } catch (error) {
-    res.status(500).json({ error: 'Logout failed' });
-  }
+    res.status(500).json({ error: "Logout failed" });
+  }
 };
